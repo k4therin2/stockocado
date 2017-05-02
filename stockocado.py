fadea19212857fa36fe7dfce49fc6dc5c72cb92a
K4therin2 <cass@case.edu>
2017-05-01 22:38:09 -0400

    readsgit add .! from NASDAQgit add .! in multiple threadsgit add . different companiesgit add .! wow!

--- stockocado.py
+++ stockocado.py
@@ -7,6 +7,8 @@
 ## [o] 5. Thread 1-4 for multiple stocks
 
 import threading
+from bs4 import BeautifulSoup
+import urllib
 
 class Stockocado():
 
@@ -20,8 +22,15 @@ class Stockocado():
 			threading.Thread(target=self._listener, args=(indexes[index],)).start()
 
 	def _listener(i, index):
-		print 'listener started on:' + index
+		print 'listener started on: ' + index
+		quote = i.get_quote(index)
+		print '[ ' + index + '] ' + str(round(quote,2))
 
-
+	def get_quote(self, index):
+		page = urllib.urlopen('http://nasdaq.com/symbol/' + index)
+		soup = BeautifulSoup(page, "html.parser")
+		quote_div = soup.find(id='qwidget_lastsale')
+		quote = float(quote_div.contents[0][1:])
+		return quote
 
 s = Stockocado()
