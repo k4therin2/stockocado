fabeec0ac7244dcdc727ad1f968022490dc518de
K4therin2 <cass@case.edu>
2017-05-01 22:13:58 -0400

    begin 4 real

---
+++ stock_reader.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import urllib
+
+symbol = 'NFLX'
+
+print "Give a NASDAQ symbol:"
+symbol = raw_input()
+
+page = urllib.urlopen('http://nasdaq.com/symbol/' + symbol)
+soup = BeautifulSoup(page, "html.parser")
+quote_div = soup.find(id='qwidget_lastsale')
+quote = float(quote_div.contents[0][1:])
+
+print quote
+
--- stockocado.py
+++ stockocado.py
@@ -1,14 +1,25 @@
-from bs4 import BeautifulSoup
-import urllib
+## TODO
 
-symbol = 'NFLX'
+## [x] 1. Price Retriever
+## [o] 2. Price Listner
+## [ ] 3. Price Grapher
+## [ ] 4. Buy/Sell decision
+## [o] 5. Thread 1-4 for multiple stocks
 
-print "Give a NASDAQ symbol:"
-symbol = raw_input()
 
-page = urllib.urlopen('http://nasdaq.com/symbol/' + symbol)
-soup = BeautifulSoup(page, "html.parser")
-quote_div = soup.find(id='qwidget_lastsale')
-quote = float(quote_div.contents[0][1:])
+class Stockocado():
 
-print quote
+	def read_indexes(self):
+		index_array = [line.rstrip('\n') for line in open('indexes.txt')]
+		return index_array
+
+	def __init__(self):
+		indexes = self.read_indexes()
+		for index in range(len(indexes)):	
+			self.start_listener_on(indexes[index])
+
+	def start_listener_on(self, index):
+		print index
+
+
+s = Stockocado()
