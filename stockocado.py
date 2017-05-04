950fa7649b0cef49e5be4a70902520f20d305de5
K4therin2 <cass@case.edu>
2017-05-04 11:10:01 -0400

    threading fixed, buy/sell logic fix

--- stockocado.py
+++ stockocado.py
@@ -2,20 +2,21 @@ import threading
 from bs4 import BeautifulSoup
 import urllib
 import time
-from time import gmtime, strftime
+from time import sleep, gmtime, strftime
 from filelock import FileLock
 
 class Stockocado():
-	global VALUE, TIME, MONTH, DAY_LOW, DAY_HIGH, symbol, toggle
+	global VALUE, TIME, DAY_LOW, DAY_HIGH, SYMBOL, lock, toggle
 	VALUE = 0
 	TIME = 1
-	MONTH = 2
-	DAY_LOW = 3
-	DAY_HIGH = 4
+	DAY_LOW = 2
+	DAY_HIGH = 3
+	SYMBOL = 4
 	toggle = 0
+	lock = threading.Lock()
 
 	def read_symbols(self):
-		symbol_array = [line.rstrip('\n') for line in open('symbols.txt')]
+		symbol_array = [line.rstrip('\n') for line in open('v_symbols.txt')]
 		return symbol_array
 
 	def __init__(self):
@@ -26,15 +27,17 @@ class Stockocado():
 		f = open('bank', 'w')
 		f.write('10000000')
 		f.close()
+		#self.lock = threading.Lock()
 
 		# each symbol gets their own thread
 		for symbol in range(len(symbols)):	
 			threading.Thread(target=self._listener, args=(symbols[symbol],)).start()
 
 	def _listener(i, symbol):
+		# get thread id 
+
 		# DEBUG OFF i.toggle = 0
-		print 'listener started on: ' + symbol
-		i.symbol = symbol		
+		print 'listener started on: ' + symbol	
 
 		# Keep track of how many shares you have	
 		f = open('my_' + symbol, 'w')
@@ -43,60 +46,76 @@ class Stockocado():
 
 		while(True):
 			# Get information (value, timestamp, monthly performance, current day high, current day low)
-			quote = i.get_quote(symbol)
+			quote = i.get_quote(symbol)			
 
-			# Print to console
-			value = quote[VALUE]
-			value = str(round(value,2))
-			timestamp = quote[TIME]
-			# print '[ ' + symbol + ' ] ' + value
+			if (len(quote) < 3):
+				# Failure if parsing issue with layout 
+				if (quote[0] == "FAILURE"):
+					print quote[1]
+					return
+				# Wait a bit if connection error, then try again
+				if (quote[0] == "IOError"):
+					print quote[1]
+					time.sleep(5)
+			else:
+				# Print to console
+				# value = quote[VALUE]
+				# value = str(round(value,2))
+				# timestamp = quote[TIME]
+				# print '[ ' + symbol + ' ] ' + value
 
-			# Save information
-			# f = open(symbol, 'a')
-			# f.write( timestamp + '\n')
-			# f.write(value + '\n')
-			# f.close()
+				# Save information
+				# f = open(symbol, 'a')
+				# f.write( timestamp + '\n')
+				# f.write(value + '\n')
+				# f.close()
 
-			# Decide if buy/sell/do nothing
-			i.process(quote)
+				# Decide if buy/sell/do nothing
+				i.process(quote)
 
-			time.sleep(1)
+				# sleep(1)
 
-	def get_quote(self, symbol):
+	def get_quote(i, symbol):
 		# Get information from siliconinvestor.com
-		page = urllib.urlopen('http://markets.siliconinvestor.com/siliconinvestor/quote?Symbol=' + symbol)
+		try:
+			page = urllib.urlopen('http://markets.siliconinvestor.com/siliconinvestor/quote?Symbol=' + symbol)
+			timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+			soup = BeautifulSoup(page, "html.parser")
+		except IOError:
+			return ["IOError", "Name Resolution Down for [" + symbol + "]."]
 
-		# GET TIME
-		timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-		soup = BeautifulSoup(page, "html.parser")
-
-		# GET VALUE
+		# GET SHARE VALUE
 		value_div = soup.find(id='quotenav1_field_price')
-		value = float(value_div.contents[0][:])
 
-		# GET MONTH PERFORMANCE
-		month = soup.find(id='quotenav1_field_price')
-		month = float(month.contents[0][:])
+		try:
+			value = float(value_div.contents[0][:])
+		except ValueError: 
+			# TEMINATE IF PAGE LAYOUT IS WEIRD
+			return ["FAILURE", 'Terminating [' + symbol + '].' ]
+
+		# GET RANGE
+		day_range = str(soup.findAll('tr', attrs={'class':'row_range'}))
+		day_range = day_range[81:]
+		day_range = day_range.split('<')
+		day_range = day_range[0].split('-')
+
+		# IF EMPTY, QUIT THREAD, PAGE LAYOUT WEIRD 
+		if (len(day_range[0]) < 1):
+			return ["FAILURE", 'Terminating [' + symbol + '].' ]
 
 		# GET LOW FOR DAY
-		day_low = soup.find(id='quotenav1_field_price')
-		day_low = float(day_low.contents[0][:])
-
+		day_low = float(day_range[0])
 		# GET HIGH FOR DAY
-		day_high = soup.find(id='quotenav1_field_price')
-		day_high = float(day_high.contents[0][:])
+		day_high = float(day_range[1])
 
-		return value, timestamp, month, day_low, day_high
+		return value, timestamp, day_low, day_high, symbol
 
 
 	def process(i, quote):
-		# IF THE STOCK IS DOING BAD LONG-RUN LEAVE IT ALONE
-		# if (month_performance < 0):
-		#	return
-		# message = "NO ACTION"
-
+		# print 'Processing [' + quote[SYMBOL] + ']: V L H ' + str(quote[VALUE]) + ' ' + str(quote[DAY_LOW]) + ' ' + str(quote[DAY_HIGH]) 
+		
 		# BUY WHEN LOW
-		if (quote[VALUE] < quote[DAY_LOW]):
+		if (quote[VALUE] == quote[DAY_LOW]):
 		# DEBUG OFF if (i.toggle == 0):
 			# message = "BOUGHT"
 			i.buy(quote)
@@ -104,9 +123,9 @@ class Stockocado():
 			return 
 
 		# SELL WHEN HIGH
-		if (quote[VALUE] > quote[DAY_HIGH]):
+		if (quote[VALUE] == quote[DAY_HIGH]):
 		# DEBUG OFF if (i.toggle == 1):
-			#message = "SOLD"
+			# message = "SOLD"
 			i.sell(quote)
 			# DEBUG OFF i.toggle = 0
 			return
@@ -116,16 +135,16 @@ class Stockocado():
 	def buy(i, quote):
 		# What is $1,000 of shares?
 		shares = 1000 / quote[VALUE]
-		print "shares to buy: " + str(shares)
+		# print "shares to buy: " + str(shares)
 
 		# open my_SYMBOL
-		f = open('my_' + i.symbol, 'r')
+		f = open('my_' + quote[SYMBOL], 'r')
 		#print "existing shares: " + f.readlines()[0][:]
 		existing_shares = float(f.readlines()[0])
 		f.close()
 
 		# add X shares		
-		f = open('my_' + i.symbol, 'w')
+		f = open('my_' + quote[SYMBOL], 'w')
 		existing_shares = existing_shares + shares
 		f.write(str(round(existing_shares,2)))
 		f.close()
@@ -134,6 +153,7 @@ class Stockocado():
 		# TODO: add file locks
 		# with FileLock('bank'):
 		if True:
+			lock.acquire()
 			f = open('bank', 'r')
 			# take out $1000 for X shares	
 			new_balance = float(f.readlines()[0]) - 1000
@@ -142,26 +162,28 @@ class Stockocado():
 			f = open('bank', 'w')
 			f.write(str(round(new_balance,2)))
 			f.close()
+			lock.release()
 
-		print 'Bought ' + i.symbol + '. ' + str(shares) + ' shares.'
+		print 'Bought [' + quote[SYMBOL] + ']. ' + str(shares) + ' shares. New balance: ' + str(round(new_balance,2))
 
-		# wait 5 minutes before doing another trade
-		time.sleep(5) 
+		# wait before doing another trade
+		sleep(30) 
 
 	def sell(i, quote):
 		# open my_SYMBOL
-		f = open('my_' + i.symbol, 'r')
+		f = open('my_' + quote[SYMBOL], 'r')
 
 		# How many shares do we have?		
 		existing_shares = float(f.readlines()[0])
 		# If we have no shares to sell, stop
 		if (existing_shares == 0):
+			print 'Tried to sell [' + quote[SYMBOL] +'] but we have no shares yet.'
 			f.close()
 			return
 
 		# Otherwise, sell
 		f.close()
-		f = open('my_' + i.symbol, 'w')
+		f = open('my_' + quote[SYMBOL], 'w')
 		f.write('0')
 		f.close()
 
@@ -169,6 +191,7 @@ class Stockocado():
 		# TODO add filelocks
 		# with FileLock("bank"):
 		if True:
+			lock.acquire()
 			f = open('bank', 'r')
 			# add value gained for X shares		
 			new_balance = float(f.readlines()[0]) + (existing_shares * quote[VALUE])
@@ -176,11 +199,10 @@ class Stockocado():
 			f = open('bank', 'w')
 			f.write(str(round(new_balance,2)))
 			f.close()
+			lock.release()
 
-		print 'Sold ' + i.symbol + '. New balance: ' + str(new_balance)
-		# wait 5 minutes before doing another trade
-		time.sleep(5)
+		print '==> SOLD ==> ' + quote[SYMBOL] + '. New balance: ' + str(new_balance)
+		# wait before doing another trade
+		# sleep(5)
 
 s = Stockocado()
-
-## Should the mather-s be in a different file? a: no i hate python
